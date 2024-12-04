14d27004c090219d0184b412d3b2fdb010f81d61
nayeli velasco <nayeserratvl@gmail.com>
2024-12-04 09:05:43 -0600

    creacion del metodo SAVE

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
+const BASE_URL= "http://localhost:8080/users"
+
 export const findAll = async () => {
 
     try {
-        const response = await axios.get("http://localhost:8080/users");
+        const response = await axios.get(BASE_URL);
         return response;
     
     } catch (error) {
@@ -11,4 +13,14 @@ export const findAll = async () => {
 
     }
     return null;
+}
+
+export const save = async ({userName, email, password}) => {
+    try {
+        return await axios.post(BASE_URL, {userName, email, password});
+
+    } catch (error) {
+        console.error("Eroor al crear el usuario: ", error);
+    }
+    return undefined;
 }
