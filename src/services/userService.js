4daf58e2cf6f30d8f6362057a18c289b27e22af6
Gabriela Ramirez Cruz <gabytacruz447@gmail.com>
2024-12-04 09:10:42 -0600

    creacion del metodo save userservice

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,21 @@
 import axios from "axios"
 
+const BASE_URL= "http://localhost:8080/users";
 export const findAll = async () => {
     try{
-        const response = await axios.get("http://localhost:8080/users");
+        const response = await axios.get(BASE_URL);
         return response;
     }catch(error){
         console.error("Error al obtener los usuarios ")
+    }
+    return null;
+}
+
+export const save = async({userName, email, password}) =>{
+    try {
+        return await axios.post(BASE_URL, {userName, email, password})
+    } catch (error) {
+        console.error("Error al crear el usuario: ", error);
     }
     return null;
 }
