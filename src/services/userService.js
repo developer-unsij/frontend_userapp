edbb5a340e905106f4b4b2b78ad1d3afaf0ce42c
LuisPerezcx <luisperez90377@gmail.com>
2024-12-04 09:05:32 -0600

    creacion del metodo save en userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,22 @@
 import axios from "axios"
 
+const BASE_URL = "http://localhost:8080/users";
+
 export const findAll = async () => {
     try{
-        const response = await axios.get("http://localhost:8080/users");
+        const response = await axios.get(BASE_URL);
         return response;
     }catch(error){
-        console.error("Error al obtener los usuarios ")
+        console.error("Error al obtener los usuarios ", error)
+    }
+    return null;
+}
+
+export const save = async ({username, email, password}) => {
+    try{
+        return await axios.post(BASE_URL, {username,email,password});
+    }catch(error){
+        console.error("Error al crear el usuario ", error);
     }
     return null;
 }
